25b204fb029f68c209cdaf447ea6b209086c6b25
Max Ilin <hellomax42@gmail.com>
2020-11-11 19:32:19 +0200

    Fixed animation speed

--- js/script.js
+++ js/script.js
@@ -2,20 +2,38 @@ window.move = function() {
 	if($('#left-door').hasClass('left-door-moved')) {
 	    $('#left-door').removeClass('left-door-moved');
 	}else{
-	    $('#left-door').addClass('left-door-moved');
-	    var delayInMilliseconds = 400; //1 second
+		
+		$('#left-door').addClass('display-block');
+
 		setTimeout(function() {
-			$('#left-door').removeClass('left-door-moved');
-		}, delayInMilliseconds);
+			$('#left-door').addClass('left-door-moved');
+		    var delayInMilliseconds = 400; //1 second
+			setTimeout(function() {
+				$('#left-door').removeClass('left-door-moved');
+
+				setTimeout(function() {
+					$('#left-door').removeClass('display-block');
+				}, 300);
+			}, delayInMilliseconds);
+		}, 100);
+
+	    
 	}
 
 	if($('#right-door').hasClass('right-door-moved')) {
 	    $('#right-door').removeClass('right-door-moved');
 	}else{
-	    $('#right-door').addClass('right-door-moved');
-	    var delayInMilliseconds = 400; //1 second
+		$('#right-door').addClass('display-block');
 		setTimeout(function() {
-			$('#right-door').removeClass('right-door-moved');
-		}, delayInMilliseconds);
+		    $('#right-door').addClass('right-door-moved');
+		    var delayInMilliseconds = 400; //1 second
+			setTimeout(function() {
+				$('#right-door').removeClass('right-door-moved');
+
+				setTimeout(function() {
+					$('#right-door').removeClass('display-block');
+				}, 300);
+			}, delayInMilliseconds);
+		}, 100);
 	}
 }
