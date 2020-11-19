010efb7d5c614a9e4e7e857760cb10aee0656899
Max Ilin <hellomax42@gmail.com>
2020-11-19 12:33:07 +0200

    Fixed buttons appearence on mobile devices

--- js/script.js
+++ js/script.js
@@ -103,6 +103,8 @@ window.move_down = function() {
 			$("#start-button-div").removeClass('hidden');
 			$("#start-button-div-mobile").removeClass('hidden');
 		}, 300);
+
+		changeFloorCounter();
 		return;
 	}
 	
